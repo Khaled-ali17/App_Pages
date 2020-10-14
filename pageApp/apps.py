37b7fa186f80224f2c52bfd7c69ee4ccf7f663ed
Khaled_AliMohamed <khaledhasham962@gmail.com>
2020-10-14 21:20:27 +0200

    initial

---
+++ pageApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PageappConfig(AppConfig):
+    name = 'pageApp'
---
+++ pageApp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomeView , AboutView
+
+urlpatterns = [
+    path("about/", AboutView.as_view(), name = "about" ),
+    path("",HomeView.as_view(), name = "home"),
+]
